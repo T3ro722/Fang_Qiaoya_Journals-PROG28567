4e5e949dda9ab8957d04d51bec69bedef7222fd2
T3ro722 <helenafang00@gmail.com>
2025-09-27 11:02:51 -0400

    Task 2B - Enemy Motion Implementation, enemies (spaceship and asteroids) will move to random places with the same acceleration method as player since they are both in space

--- week_3/Journal_3/Assets/Scripts/Controllers/Enemy.cs
+++ week_3/Journal_3/Assets/Scripts/Controllers/Enemy.cs
@@ -3,10 +3,33 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Enemy Movement Settings")]
+    public float EmoveSpeed = 0.5f;
+    public float EmaxSpeed = 3f;
+    public float EaccelerationTime = 1f;
     
+
+    private Vector3 Evelocity;
+    private float Eacceleration;
+    private Vector3 EtargetPosition;
+    private Vector3 EtargetDirection;
+
+
     private void Update()
     {
-        
+        EnemyMovement();
     }
 
+    private void EnemyMovement()
+    {
+        Eacceleration = EmaxSpeed / EaccelerationTime;
+
+        EtargetPosition = new Vector3(Random.Range(-60f, 60f), Random.Range(-60f, 60f), 0);
+        EtargetDirection = (EtargetPosition - transform.position).normalized;
+        Evelocity += EtargetDirection * Eacceleration * Time.deltaTime;
+
+        Evelocity = Vector3.ClampMagnitude(Evelocity, EmaxSpeed);
+        transform.position += Time.deltaTime * Evelocity;
+
+    }
 }
