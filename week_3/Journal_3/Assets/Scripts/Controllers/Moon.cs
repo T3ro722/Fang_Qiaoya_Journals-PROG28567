01ff2b39875f16f9cf4b9a4c1a4da7dde25d6524
T3ro722 <helenafang00@gmail.com>
2025-10-04 15:34:36 -0400

    Task 3 - moon orbit around an object in the scene, by float radius (distance to the object), float speed (how fast)

--- week_3/Journal_3/Assets/Scripts/Controllers/Moon.cs
+++ week_3/Journal_3/Assets/Scripts/Controllers/Moon.cs
@@ -15,6 +15,17 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
+            OrbitalMotion(1.0f, 5.0f, planetTransform);
+        
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        float angle = Time.time * speed;
+        float x = target.position.x + radius * Mathf.Cos(angle);
+        float y = target.position.y + radius * Mathf.Sin(angle);
+        transform.position = new Vector3(x, y, 0);
     }
 }
